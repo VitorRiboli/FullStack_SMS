53b0b07935bb0da0710432a0b71ad396af7314cd
Vitor Riboli <vitorriboli1@gmail.com>
2022-09-07 14:08:19 -0300

    Project Created

---
+++ backend/src/test/java/com/vitor/sendsms/SendsmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vitor.sendsms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SendsmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
